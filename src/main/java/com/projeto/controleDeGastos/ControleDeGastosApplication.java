e48280ff124f4d168ba511c5df5924b288cd9548
Isabella Lima <isabellalimasilva@gmail.com>
2025-01-30 20:53:59 -0300

    Ajustes para iniciar a configuração

---
+++ src/main/java/com/projeto/controleDeGastos/ControleDeGastosApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.controleDeGastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeGastosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/controleDeGastos/ControleDeGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.controleDeGastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
